43cd99d70d19d5dc90341e38a18b9ffe458ad048
Abdul Aziz <aapppp742@gmail.com>
2025-11-28 11:28:31 +0500

    update

--- QrGenerater/main.py
+++ QrGenerater/main.py
@@ -3,10 +3,15 @@ import qrcode
 url = input("Enter the URL to generate QR code: ").strip()
 file_path = "D:/Git Repository/PyFusion/QrGenerater/qrcode.png"
 
-qr = qrcode.QRCode()
+qr = qrcode.QRCode(
+    # version=1,
+    # error_correction=qrcode.constants.ERROR_CORRECT_H,
+    # box_size=10,
+    # border=4,
+)
 
 qr.add_data(url)
 qr.make(fit=True)
 
-img = qr.make_image(fill_color="white", back_color="black")
+img = qr.make_image(fill_color="black", back_color="white")
 img.save(file_path)
