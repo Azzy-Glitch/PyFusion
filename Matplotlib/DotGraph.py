bc2d2de43099697422c3f9c01fa38ddb515d2332
Abdul Aziz <aapppp742@gmail.com>
2026-01-31 09:22:09 +0500

    Update

--- Matplotlib/DotGraph.py
+++ Matplotlib/DotGraph.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
+xpoints = np.array([1, 8, 10])
+ypoints = np.array([3, 10, 15])
 
 plt.plot(xpoints, ypoints, 'o')
 plt.show()
